503bc6098e94304561fb153348ad81652ab321c9
Andrew Khrobostov <andrew.khrobostov@gmail.com>
2026-03-30 15:45:32 +0300

    Add few method implementations and few private methods
    Добавлена реализация для:
    - done?
    - cancel
    и пара вспомогательных ф-ций

--- lib/context.rb
+++ lib/context.rb
@@ -22,14 +22,53 @@ class Context
     @children = []
   end
 
-  # пока что моки для первой итерации
-  # background никогда не завершён
+  # Проверяет завершен ли контекст
+  # Для background (не cancelable) всегда возвращает false
+  # Проверяет отмену, deadline и родительские контексты
   def done?
-    false
+    return false unless @cancelable
+
+    @mutex.synchronize do
+      return true if @canceled
+      return true if deadline_exceeded?
+      return true if parent_done?
+      false
+    end
+  end
+
+  # Отменяет контекст
+  # Для background (не cancelable) ничего не делает
+  # Устанавливает @canceled = true и @err = CanceledError
+  # Рекурсивно отменяет всех детей
+  def cancel
+    return unless @cancelable
+
+    @mutex.synchronize do
+      return if @canceled
+
+      @canceled = true
+      @err = CanceledError.new("context canceled")
+      
+      # Отменяем всех детей
+      @children.each(&:cancel)
+      @children.clear
+    end
   end
 
   # background никогда не содержит ошибок
   def err
     nil
+  end
+
+  private
+
+  # Проверяет, истек ли deadline
+  def deadline_exceeded?
+    @deadline && Time.now >= @deadline
+  end
+
+  # Проверяет, завершен ли родительский контекст
+  def parent_done?
+    @parent&.done? || false
   end
 end
